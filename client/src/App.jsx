dbe95ef3084620c1ac029c8f1fbc78401a14e953
Leoguzs <lguzs96@gmail.com>
2021-11-18 16:38:35 -0600

    estructura inicial del proyecto lista

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannellListContainer, ChannellContainer } from "./components";
+
+import "./App.css";
+
+const apiKey = "c775t29nwa2b";
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>BEDU Music</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team dark">
+        <ChannellListContainer />
+        <ChannellContainer />
+      </Chat>
     </div>
   );
 };
---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as ChannellListContainer } from "./ChannellListContainer";
+export { default as ChannellContainer } from "./ChannellContainer";
+export { default as ChannellSearch } from "./ChannellSearch";
+export { default as TeamChannelList } from "./TeamChannelList";
+export { default as TeamChannelPreview } from "./TeamChannelPreview";
