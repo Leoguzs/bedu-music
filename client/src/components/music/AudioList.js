5cdcdbbe4df9aa6d1bed276fdbccbd12a0923f6e
Leoguzs <lguzs96@gmail.com>
2021-12-16 16:32:08 -0600

    estilos en canciones

--- client/src/components/music/AudioList.js
+++ client/src/components/music/AudioList.js
@@ -1,40 +1,48 @@
 import React from "react";
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import '../music/css/AudioList.css'
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import "../music/css/AudioList.css";
 
-const AudioList = ({ tracks, trackIndex, setTrackIndex, playlist, handleClickUpdateTrackIndex }) => {
-    return (
-        < div className="playlist no_drag" >
-            {//console.log(playlist)
-            }
-            < ul className="lista" style={{textDecoration:'none'}}>
-                {
-                    playlist.tracks.map((track, i) => (
-                        <li
-                            className={'tracklist'}
-                            key={i}
-                            style={{textDecoration:'none'}}
-                        >
-
-                            <div className="playlist_btns_group">
-                                <Button variant="contained" 
-                                    color="secondary"
-                                    onClick={(e) => {
-                                        handleClickUpdateTrackIndex(e, i);
-                                    }}
-                                    className="fav_song playlist_btn">
-                                    <i>play</i>
-                                </Button>
-                                <span className="trackname">{track.name}<br /></span>
-                            </div>
-                        </li>
-                    ))
-                }
-            </ul >
-        </div >
-    );
+const AudioList = ({
+  tracks,
+  trackIndex,
+  setTrackIndex,
+  playlist,
+  handleClickUpdateTrackIndex,
+}) => {
+  return (
+    <div className="playlist no_drag">
+      {
+        //console.log(playlist)
+      }
+      <ul className="lista" style={{ textDecoration: "none" }}>
+        {playlist.tracks.map((track, i) => (
+          <li
+            className={"tracklist"}
+            key={i}
+            style={{ textDecoration: "none" }}
+          >
+            <div className="playlist_btns_group">
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={(e) => {
+                  handleClickUpdateTrackIndex(e, i);
+                }}
+                className="fav_song playlist_btn"
+              >
+                <i>play</i>
+              </Button>
+              <span className="trackname">
+                {track.name}
+                <br />
+              </span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
-
 
 export default AudioList;
